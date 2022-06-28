f5e3f064030c3cf45cadea8df71a98530e3d9fe3
umamaheshrao <96564185+umamaheshrao@users.noreply.github.com>
2022-06-28 23:30:35 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,n-1):             print(j,end="")     for k in reversed (range(1,j)):         print(k,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,6 +2,6 @@ n=int(input())
 for i in range(1,n+1):
     for j in range(1,n-1):
             print(j,end="")
-    for k in range(1,j):
+    for k in reversed (range(1,j)):
         print(k,end="")
     print()
