90838fc70a6cf7424c33a13bb94d060274c6f2f0
umamaheshrao <96564185+umamaheshrao@users.noreply.github.com>
2022-10-06 22:39:26 +0530

    n=input() n=n.lower() z=0 a=[] for i in n:     if(n.count(i)==1 and i!=" "):         a.append(i)         z=1 a=sorted(a) x="".join(a) if(z==0):     print("-1") else:     print(x)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,8 +1,15 @@
 n=input()
 n=n.lower()
-n=set(n)
-n=sorted(n)
+z=0
+a=[]
 for i in n:
-    if(i==" "):
-        continue
-    print(i,end="")
+    if(n.count(i)==1 and i!=" "):
+        a.append(i)
+        z=1
+a=sorted(a)
+x="".join(a)
+if(z==0):
+    print("-1")
+else:
+    print(x)
+    
